2797de93f8cb9d640f8ee826fbb64ede66cee056
7estelle <layestelle7@gmail.com>
2023-10-30 19:27:59 +0100

    feat: add credits modal and favicon

--- sources/index.js
+++ sources/index.js
@@ -5,3 +5,23 @@ const experience = new Experience({
     targetElement: document.querySelector('.experience')
 })
 
+// Credits
+
+const modal = document.getElementById("creditsModal");
+const btn = document.getElementById("creditsBtn");
+
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
